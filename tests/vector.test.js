a3406dccec4bf33175f5127fb99f8e5ad93d4671
Shail <shailpatel67@gmail.com>
2020-07-07 23:59:29 -0400

    add vector test

---
+++ tests/vector.test.js
@@ -0,0 +1,15 @@
+
+import {Vector} from '../src/lib/vector.js';
+import {Point} from '../src/lib/point.js';
+
+describe('Vector tests', () => {
+	it('create vector', () => {
+		let v = new Vector(
+			new Point(0,0), new Point(0,1)
+		);
+
+		expect(v.r).toBe(1);
+		expect(v.left_to_right).toBe(true);
+		expect(v.angle_degrees).toBe(90);
+	});
+});
